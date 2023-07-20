08163873252705777d8276d6d3311a27f5ce178c
carlosweb07 <carlosmirabalv@gmail.com>
2023-07-19 21:13:23 -0400

    Connected the modules with the controllers.

--- controllers/huesped.py
+++ controllers/huesped.py
@@ -1,9 +1,12 @@
-import models as models
+from models.huesped import Huesped
 import re
+
+huesped_modelo = Huesped()
 
 class huesped():
     
     def insert(self, ced_hue, ape_hue, nom_hue, dir_hue, ciu_hue, email_hue, tel_hue):
+
         try:
             if not ced_hue or not ape_hue or not nom_hue or not dir_hue or not ciu_hue or not email_hue or not tel_hue:
                 return "Faltan campos por llenar, por favor ingrese todos los datos que se le estan pidiendo."
@@ -24,7 +27,52 @@ class huesped():
             
             if tel_hue < 1000000000 or tel_hue > 10000000000:
                 return "El numero de telefono no puede ser mayor ni menor a 11 digitos."
+            
+            result = huesped_modelo.insert(ced_hue, ape_hue, nom_hue, dir_hue, ciu_hue, email_hue, tel_hue)
 
+            return result
 
         except Exception as error:
             return error
+        
+    def select_all(self):
+        todos_hue = huesped_modelo.select_all()
+        return todos_hue
+    
+    def select_one(self, ced_hue):
+        hue = huesped_modelo.select_one(ced_hue)
+        return hue
+    
+    def update(self, ced_hue, ape_hue, nom_hue, dir_hue, ciu_hue, email_hue, tel_hue):
+        
+        try:
+            if not ced_hue or not ape_hue or not nom_hue or not dir_hue or not ciu_hue or not email_hue or not tel_hue:
+                return "Faltan campos por llenar, por favor ingrese todos los datos que se le estan pidiendo."
+
+            if not isinstance(ced_hue, int):
+                return "La cedula no puede tener puntos decimales."
+            
+            if ced_hue < 100000 or ced_hue > 100000000:    
+                return 'La cedula tiene que estar en el rango de 6 o 8 digitos.'
+
+            validar_correo = r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"
+            
+            if not re.match(validar_correo, email_hue):
+                return 'El correo electronico no es valido, intentelo nuevamente.'
+            
+            if not isinstance(tel_hue, int):
+                return "El numero de telefono no puede tener letras, simbolos o signos."
+            
+            if tel_hue < 1000000000 or tel_hue > 10000000000:
+                return "El numero de telefono no puede ser mayor ni menor a 11 digitos."
+            
+            result = huesped_modelo.update(ced_hue, ape_hue, nom_hue, dir_hue, ciu_hue, email_hue, tel_hue)
+
+            return result
+
+        except Exception as error:
+            return error
+        
+    def delete(self, ced_hue):
+        result = huesped_modelo.delete(ced_hue)
+        return result
--- controllers/ingreso.py
+++ controllers/ingreso.py
@@ -1,5 +1,7 @@
-import models as models
+from models.ingreso import Ingreso
 from datetime import datetime
+
+ingreso_modelo = Ingreso()
 
 class ingreso():
     
@@ -23,3 +25,42 @@ class ingreso():
         
         else:
             print("¡La fecha es correcta!")
+
+            result = ingreso_modelo.insert(ced_hue, cod_hab, fec_sal, can_per)
+            return result
+    
+    def select_all(self):
+        todos_ing = ingreso_modelo.select_all()
+        return todos_ing
+    
+    def select_one(self, cod_ing):
+        ing = ingreso_modelo.select_one(cod_ing)
+        return ing
+    
+    def update(self, ced_hue, cod_hab, fec_sal, can_per):
+        try:
+            
+            if not ced_hue or not cod_hab or not fec_sal or not can_per:
+                return "Faltan campos por llenar, por favor ingrese todos los datos que se le estan pidiendo."
+            
+            if not isinstance(ced_hue, int):
+                return "La cedula del huesped no puede tener puntos decimales."
+            
+            if not isinstance(can_per, int):
+                return "La cantidad de personas no puede tener letras."
+
+            fec_sal = datetime.datetime.strptime(fec_sal, "%d/%m/%Y").date()
+        
+        except ValueError:
+
+            print("La fecha es inválida, debe estar e el formato dd/mm/aaaa, por ejemplo: 19/07/2023")
+        
+        else:
+            print("¡La fecha es correcta!")
+
+            result = ingreso_modelo.update(ced_hue, cod_hab, fec_sal, can_per)
+            return result
+    
+    def delete(self, cod_ing):
+        result = ingreso_modelo.delete(cod_ing)
+        return result
